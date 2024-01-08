297210ce3b44115beda78fced76fdb170eb22f7d
Lucie <lucye.laury@gmail.com>
2024-01-08 11:06:57 +0100

    renommage

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { TransformControls } from 'three/addons/controls/TransformControls.js';
-import { convertirSTLtoDonnees } from './tool/tool.js';
+import { convertirSTLtoDonnees } from './tool/DataStructureImplementation.js';
 import { Face } from './structure/Face.js';
 import { Mesh } from "./structure/Mesh";
 import { HalfEdge } from "./structure/HalfEdge";
