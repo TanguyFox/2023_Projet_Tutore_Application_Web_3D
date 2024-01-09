a3dcc1156aad91a4cee73850bcedbbdbe116f0fd
Lucie <lucye.laury@gmail.com>
2024-01-09 15:24:43 +0100

    intégration des deux volets de modification (visualisation et modification)

--- js/vue/ModificationMenuVue.js
+++ js/vue/ModificationMenuVue.js
@@ -2,3 +2,22 @@
  * module gérant le menu de modification
  */
 
+let listeButton = document.querySelectorAll("#headerMenu li");
+let listeContent = document.querySelectorAll(".contentModifMenu");
+
+function changerPannel(){
+    console.log(this);
+
+    let nom = this.id.replace(/_button/g, '');
+    let contenu = document.querySelector(`#${nom}_content`);
+    console.log(contenu)
+
+    listeContent.forEach((e)=>
+        e.style.display="none"
+    )
+    contenu.style.display = "flex";
+}
+
+listeButton.forEach((elem) =>
+        elem.addEventListener('click', changerPannel)
+);
