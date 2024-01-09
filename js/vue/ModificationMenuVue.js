e5790247227a0beed50ed1e50d55a946e8045cee
Lucie <lucye.laury@gmail.com>
2024-01-09 17:05:06 +0100

    animation bouton du menu entre les onglet

--- js/vue/ModificationMenuVue.js
+++ js/vue/ModificationMenuVue.js
@@ -4,20 +4,42 @@
 
 let listeButton = document.querySelectorAll("#headerMenu li");
 let listeContent = document.querySelectorAll(".contentModifMenu");
+let clicked;
+let valeurTranslateIndicatorPrecedente;
+const indicator = document.querySelector('#indicator');
 
 function changerPannel(){
-    console.log(this);
-
     let nom = this.id.replace(/_button/g, '');
     let contenu = document.querySelector(`#${nom}_content`);
-    console.log(contenu)
-
     listeContent.forEach((e)=>
         e.style.display="none"
     )
     contenu.style.display = "flex";
+    clicked = true;
 }
 
 listeButton.forEach((elem) =>
         elem.addEventListener('click', changerPannel)
 );
+
+function survolerIndicator(){
+    let index = Array.from(listeButton).indexOf(this);
+    let nouvelleValeur = 155*index;
+    valeurTranslateIndicatorPrecedente = indicator.style.transform;
+    indicator.style.transform = 'translateX(calc( ' + nouvelleValeur + 'px))';
+    clicked = false;
+}
+
+listeButton.forEach((element)=>
+    element.addEventListener('mouseover', survolerIndicator)
+);
+
+function retablirIndicator(){
+    if(!clicked){
+        indicator.style.transform = valeurTranslateIndicatorPrecedente;
+    }
+}
+
+listeButton.forEach((ele) =>
+    ele.addEventListener('mouseout', retablirIndicator)
+)
