7ff0c26038e867a28e14a236ad0de25c54ef092f
Lucie <lucye.laury@gmail.com>
2024-01-23 17:53:28 +0100

    A peine moins d'erreur à la création de point mais c'est toujours pas opé

--- js/fonctionnalites/AjoutPoint.js
+++ js/fonctionnalites/AjoutPoint.js
@@ -11,7 +11,7 @@ import * as Scene3D from "../vue/Scene3D";
 import * as Raycaster from "../tool/Raycaster";
 import {int} from "three/nodes";
 import {initEventInputCoord} from "../controleurs/ModificationMenu";
-import {geometry_model, groupAsWireframe, mesh} from "../tool/Element3DGeneraux";
+import {geometry_model, groupAsWireframe, mesh, meshModel} from "../tool/Element3DGeneraux";
 import {Point} from "../structure/Point";
 import {majEdges} from "./ModifCoordPoint";
 import {camera} from "../vue/Scene3D";
@@ -25,11 +25,21 @@ export function ajoutPoint(menuContextuel){
     //initEventInputCoord();
    // remplirStructureDeDonnees(sphere.position);
     let newPoint = new Point(sphere.position.x, sphere.position.y, sphere.position.z);
-    let troisPointsProches = trouver3ptsProche(newPoint);
-    majBufferGeometry(newPoint, troisPointsProches[0].point, troisPointsProches[1].point);
-    majBufferGeometry(newPoint, troisPointsProches[1].point, troisPointsProches[2].point);
-    majBufferGeometry(newPoint, troisPointsProches[0].point, troisPointsProches[2].point);
+    let troisPointsProches = trouver3ptsProches2(newPoint);
+    console.log(troisPointsProches);
+    //supprimerAncienneFace(troisPointsProches);
+    majBufferGeometry(newPoint, troisPointsProches[2], troisPointsProches[1]);
+    majBufferGeometry(newPoint, troisPointsProches[1], troisPointsProches[0]);
+    majBufferGeometry(newPoint, troisPointsProches[0], troisPointsProches[2]);
+    geometry_model.computeBoundingSphere(); // Recalcul du sphere de la bounding box
+    geometry_model.computeBoundingBox(); // Recalcul de la bounding box
+    //geometry_model.computeFaceNormals(); // Recalcul des normales des faces
+    geometry_model.computeVertexNormals();
+    geometry_model.attributes.position.needsUpdate = true;
+    meshModel.geometry = geometry_model;
+    Scene3D.transformControls.detach();
 
+    majEdges();
 }
 
 /**
@@ -78,14 +88,16 @@ function ajPoint3D(){
     }
 
     Scene3D.scene.add(sphere);
-    console.log(sphere.uuid)
+   /* console.log(sphere.uuid)
     console.log(Scene3D.scene)
+
+    */
 
 
     return sphere;
 }
 
-function trouver3ptsProche(newPoint){
+function trouver3ptsProches(newPoint){
     //trouver les trois points les plus proches
     let troisPoints = [];
     let pointsDejaAjoutes = new Set();
@@ -117,16 +129,38 @@ function trouver3ptsProche(newPoint){
     // Trier le tableau en fonction de la distance croissante
     troisPoints.sort((a, b) => a.distance - b.distance);
 // Sélectionner les trois premiers points du tableau trié
+
     troisPoints = troisPoints.slice(0, 3);
+    troisPoints = troisPoints.map((element) => element.point);
 
     return troisPoints;
 }
 
+function trouver3ptsProches2(newPoint){
+    let positions = Array.from(generaux.geometry_model.getAttribute("position").array);
+    let distances =[] ;
+    console.log(positions);
+    for(let i = 0 ; i<positions.length; i+=9){
+        console.log(i);
+        let p1 = new Point(positions[i], positions[i + 1], positions[i + 2]);
+        let p2 = new Point(positions[i + 3], positions[i + 4], positions[i + 5]);
+        let p3 = new Point(positions[i + 6], positions[i + 7], positions[i + 8]);
+
+        let distanceP1 = p1.distance(newPoint);
+        let distanceP2 = p2.distance(newPoint);
+        let distanceP3 = p3.distance(newPoint);
+        let distanceTotale = distanceP1+distanceP2+distanceP3;
+        distances.push({distance : distanceTotale, points:[p3,p2,p1]});
+    }
+    distances.sort((a,b) => a.distance - b.distance);
+    return distances[0].points;
+}
+
 function majBufferGeometry(newPoint, p1, p2){
     let positionAttribute = geometry_model.attributes.position;
-    console.log(geometry_model.attributes.position)
+    //console.log(geometry_model.attributes.position)
     let positions = Array.from(positionAttribute.array);
-    console.log(positions);
+    //console.log(positions);
 
     /*
     Il faut rentrer trois points sinon ça bug ! donc je dois trouver les points les plus proches au nouveau
@@ -146,15 +180,34 @@ function majBufferGeometry(newPoint, p1, p2){
 
     //console.log(positions)
     geometry_model.setAttribute( 'position', new THREE.BufferAttribute( new Float32Array(positions), 3 ) );
-    console.log(geometry_model);
+   // console.log(geometry_model);
+}
 
-    geometry_model.computeBoundingSphere(); // Recalcul du sphere de la bounding box
-    geometry_model.computeBoundingBox(); // Recalcul de la bounding box
-    //geometry_model.computeFaceNormals(); // Recalcul des normales des faces
-    geometry_model.computeVertexNormals();
-    geometry_model.attributes.position.needsUpdate = true;
-    Scene3D.transformControls.detach();
-    majEdges();
+function supprimerAncienneFace(troisPoints){
+    console.log('dans suppression')
+    console.log(troisPoints);
+    let positions = Array.from(generaux.geometry_model.getAttribute("position").array);
+    console.log(positions);
+    for(let i = 0 ; i<positions.length; i+=9){
+        console.log(i);
+        let p1 = new Point(positions[i], positions[i + 1], positions[i + 2]);
+        let p2 = new Point(positions[i + 3], positions[i + 4], positions[i + 5]);
+        let p3 = new Point(positions[i + 6], positions[i + 7], positions[i + 8]);
+
+        if(includePoint(troisPoints, p1) && includePoint(troisPoints, p2) && includePoint(troisPoints, p3)){
+            console.log('contains point à i = ' + i);
+            positions.splice(i, i+9);
+            geometry_model.setAttribute('position', new THREE.BufferAttribute(new Float32Array(positions), 3));
+        }
+    }
+}
+
+function includePoint(liste, point){
+    let include = false
+    liste.forEach((pointCourant)=>{
+        if(pointCourant.equals(point)) include = true;
+    });
+    return include;
 }
 
 function remplirStructureDeDonnees(coordonnees){
