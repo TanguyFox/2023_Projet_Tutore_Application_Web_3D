917c4b4030a9afdef72f3d2c5e9e9017433459c6
TanguyFox <tanguyfox@gmail.com>
2024-01-25 09:59:26 +0100

    Detection des trous potentiels

--- js/structure/Mesh.js
+++ js/structure/Mesh.js
@@ -30,7 +30,7 @@ Mesh.prototype.setMeshGeneraux = async function (){
 
 Mesh.prototype.highlightEdge = function () {
 
-    this.badHalfEdges.trous.forEach(he => {
+    this.badHalfEdges.forEach(he => {
         let edges = [];
         edges.push(new THREE.Vector3(he.headVertex().point.x, he.headVertex().point.y, he.headVertex().point.z));
         edges.push(new THREE.Vector3(he.tailVertex().point.x, he.tailVertex().point.y, he.tailVertex().point.z));
@@ -38,15 +38,6 @@ Mesh.prototype.highlightEdge = function () {
             let edgesLine = new THREE.LineSegments(edgesGeometry, new THREE.LineBasicMaterial({color: "rgb(250,26,26)", linewidth : 10}));
             group.add(edgesLine);
     });
-
-    this.badHalfEdges.duplicateHalfedges.forEach(he => {
-        let dup = [];
-        dup.push(new THREE.Vector3(he.headVertex().point.x, he.headVertex().point.y, he.headVertex().point.z));
-        dup.push(new THREE.Vector3(he.tailVertex().point.x, he.tailVertex().point.y, he.tailVertex().point.z));
-        let dupGeometry = new THREE.BufferGeometry().setFromPoints(dup);
-            let dupLine = new THREE.LineSegments(dupGeometry, new THREE.LineBasicMaterial({color: "rgb(129,0,253)", linewidth : 10}));
-            group.add(dupLine);
-    })
     console.log(group);
 }
 
