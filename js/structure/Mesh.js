8018f4e7ae8885f2e81b94f6fea00b034da84bb4
TanguyFox <tanguyfox@gmail.com>
2024-01-25 09:48:37 +0100

    Ajout methode equals

--- js/structure/Mesh.js
+++ js/structure/Mesh.js
@@ -30,14 +30,23 @@ Mesh.prototype.setMeshGeneraux = async function (){
 
 Mesh.prototype.highlightEdge = function () {
 
-    this.badHalfEdges.forEach(he => {
+    this.badHalfEdges.trous.forEach(he => {
         let edges = [];
         edges.push(new THREE.Vector3(he.headVertex().point.x, he.headVertex().point.y, he.headVertex().point.z));
         edges.push(new THREE.Vector3(he.tailVertex().point.x, he.tailVertex().point.y, he.tailVertex().point.z));
         let edgesGeometry = new THREE.BufferGeometry().setFromPoints(edges);
-            let edgesLine = new THREE.LineSegments(edgesGeometry, new THREE.LineBasicMaterial({color: 0xff0000, linewidth : 10}));
+            let edgesLine = new THREE.LineSegments(edgesGeometry, new THREE.LineBasicMaterial({color: "rgb(250,26,26)", linewidth : 10}));
             group.add(edgesLine);
     });
+
+    this.badHalfEdges.duplicateHalfedges.forEach(he => {
+        let dup = [];
+        dup.push(new THREE.Vector3(he.headVertex().point.x, he.headVertex().point.y, he.headVertex().point.z));
+        dup.push(new THREE.Vector3(he.tailVertex().point.x, he.tailVertex().point.y, he.tailVertex().point.z));
+        let dupGeometry = new THREE.BufferGeometry().setFromPoints(dup);
+            let dupLine = new THREE.LineSegments(dupGeometry, new THREE.LineBasicMaterial({color: "rgb(129,0,253)", linewidth : 10}));
+            group.add(dupLine);
+    })
     console.log(group);
 }
 
