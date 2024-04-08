518da54074e4c237b3a3991c52a75a26bf384877
Lucie <lucye.laury@gmail.com>
2024-04-08 12:51:45 +0200

    première version du remplissage des trous

--- js/fonctionnalites/RemplissageTrouCirculaire.js
+++ js/fonctionnalites/RemplissageTrouCirculaire.js
@@ -39,23 +39,25 @@ export function remplirTrouTotal(tableauDeTrous) {
  */
 function remplirTrouRepartisseur(tableauDeVertex) {
     //let tableau = preparerTableau(tableauDeVertex);
-    let tableau = sensTableau(tableauDeVertex);
+    //let tableau = sensTableau(tableauDeVertex);
+    let tableau = tableauDeVertex;
     console.log(tableau);
     if (tableau.length !== 0) {
         if (tableau.length % 2 === 0) {
             //tableau pair
-            //remplirTrouTableauPair(tableau);
-            remplirTrouAll(tableau, remplirTrouTableauPair2, verifTabPair);
+            remplirTrouTableauPair(tableau);
+            //remplirTrouAll(tableau, remplirTrouTableauPair2, verifTabPair);
         } else {
             //tableau impair
-            //remplirTrouTableauImpair(tableau);
-            remplirTrouAll(tableau, remplirTrouTableauImpair2, verifTabImpair);
+            remplirTrouTableauImpair(tableau);
+            //remplirTrouAll(tableau, remplirTrouTableauImpair2, verifTabImpair);
         }
     }
 
 }
 
 function sensTableau(tableauTrou){
+    console.log("recherche du sens du tableau", tableauTrou);
     let point = tableauTrou[0];
     console.log(point.point);
     let halfedge = mesh.getHalfedgeOfVertexWithoutOpposite(point);
@@ -73,9 +75,11 @@ function sensTableau(tableauTrou){
         if(tableauTrou.includes(ptsDep)){
             let indexPtsDep = tableauTrou.indexOf(ptsDep);
             if(indexPtsDep===1){
+                console.log("inversion du tableau")
                 return preparerTableau(tableauTrou);
             }
             if (indexPtsDep===tableauTrou.length-1){
+                console.log("non inversion du tableau")
                 return tableauTrou;
             } else {
                 throw new Error ("impossible de determiner le sens de parcours");
@@ -128,7 +132,8 @@ function remplirTrouAll(tableauDeTrous, fonctionDo, fonctionVerification){
     }
 }
 
-function completerStructure(tabUneFace){
+/*function completerStructure(tabUneFace){
+    console.log("Dans completer structure", tabUneFace)
     let v1 = tabUneFace[0];
     let v2 = tabUneFace[1];
     let v3 = tabUneFace[2];
@@ -141,20 +146,45 @@ function completerStructure(tabUneFace){
     if(typeof h === 'undefined'){
         throw new Error("la face nouvellement créé n'a pas d'opposé")
     }
+    console.log("halfedge trouvee", h);
     let pointDep = h.next.vertex;
     let pointSuivant = h.vertex;
     let dernierPoint = trouverTroisiemePoint(tabUneFace, pointDep, pointSuivant);
     let opposeeDirect = new HalfEdge(pointDep);
     opposeeDirect.opposite = h;
+    h.opposee = opposeeDirect;
     let h2 = new HalfEdge(pointSuivant);
     let h3 = new HalfEdge(dernierPoint);
+    console.log("etat de la face avant remplissage : ", opposeeDirect, h2, h3);
+
     let newFace = new Face(opposeeDirect);
-    mesh.faces.add(newFace);
+
     setnextAndPrev(opposeeDirect, h2, h3);
     setnextAndPrev(h2, h3, opposeeDirect);
     setnextAndPrev(h3, opposeeDirect, h2);
-    //TODO
-}
+
+    opposeeDirect.face = newFace;
+    h2.face = newFace;
+    h3.face = newFace;
+    console.log("etat de la face après remplissage : ", opposeeDirect, h2, h3);
+    //TODO trouver les halfedges opposees de H2 et H3
+    console.log("remplissage des opposees de h2 et h3");
+    let halfedgeOpposeeDeH2 = mesh.getHalfedgeBetween2VertexWithoutOpposite(h2.vertex, h2.next.vertex);
+    console.log("halfedgeDeH2", halfedgeOpposeeDeH2);
+    if(typeof halfedgeOpposeeDeH2 !== "undefined"){
+        h2.opposite = halfedgeOpposeeDeH2;
+        halfedgeOpposeeDeH2.opposite = h2;
+    }
+
+    let halfedgeOpposeeDeH3 = mesh.getHalfedgeBetween2VertexWithoutOpposite(h3.vertex, h3.next.vertex);
+    console.log("halfedgeDeH3", halfedgeOpposeeDeH3);
+    if(typeof halfedgeOpposeeDeH3 !== "undefined"){
+        h3.opposite = halfedgeOpposeeDeH3;
+        halfedgeOpposeeDeH3.opposite = h3;
+    }
+    console.log("etat de la face après remplissage des opposees : ", opposeeDirect, h2, h3);
+    mesh.faces.push(newFace);
+}*/
 function setnextAndPrev(h1, h2, h3){
     h1.next = h2;
     h1.prev = h3;
@@ -162,25 +192,22 @@ function setnextAndPrev(h1, h2, h3){
 
 
 function trouverTroisiemePoint(tableauPoints, point1, point2) {
-    // Boucler à travers les points dans le tableau
+
     for (let i = 0; i < tableauPoints.length; i++) {
-        // Vérifier si le point courant est différent des deux points donnés
-        if (tableauPoints[i].x !== point1.x || tableauPoints[i].y !== point1.y || tableauPoints[i].z !== point1.z) {
-            if (tableauPoints[i].x !== point2.x || tableauPoints[i].y !== point2.y || tableauPoints[i].z !== point2.z) {
-                // Si le point courant est différent des deux points donnés, retourner ce point
-                return tableauPoints[i];
-            }
+        const currentPoint = tableauPoints[i];
+        // Vérifier si le point courant n'est ni point1 ni point2
+        if (!currentPoint.equals(point1) && !currentPoint.equals(point2)) {
+            return currentPoint;
         }
     }
-
-    // Retourner null si aucun point n'a été trouvé
+    // Retourner null si le troisième point n'a pas été trouvé
     return null;
 }
 /**
  * méthode qui rempli un tableau de trous ayant une longueur paire. Renvoie le tableau de trou restant.
  * @param tableauDeTrous
  */
-/*function remplirTrouTableauPair(tableauDeTrous) {
+function remplirTrouTableauPair(tableauDeTrous) {
     console.log("dans remplissage tableau pair")
     let indexCourant = 0;
     let pointDebutFace = tableauDeTrous[indexCourant];
@@ -208,7 +235,7 @@ function trouverTroisiemePoint(tableauPoints, point1, point2) {
     if (tableauProchainRemplissage.length >= 3) {
         remplirTrouRepartisseur(tableauProchainRemplissage);
     }
-}*/
+}
 function remplirTrouTableauPair2(tableauDeTrous, tabUneFace, pointDebutFace, indexCourant){
     if (typeof tableauDeTrous[indexCourant + 2] == 'undefined') {
         tabUneFace.push(pointDebutFace, tableauDeTrous[indexCourant + 1], tableauDeTrous[0]);
@@ -231,7 +258,7 @@ function verifTabPair(tableauDeTrous, pointDebutFace){
  * @param tableauDeTrous
  * @returns {*}
  */
-/*function remplirTrouTableauImpair(tableauDeTrous) {
+function remplirTrouTableauImpair(tableauDeTrous) {
     console.log("dans remplissage tableau impair")
     if (tableauDeTrous.length === 3) {
         remplirGeometry(tableauDeTrous);
@@ -257,7 +284,7 @@ function verifTabPair(tableauDeTrous, pointDebutFace){
             remplirTrouRepartisseur(tableauProchainRemplissage);
         }
     }
-}*/
+}
 function remplirTrouTableauImpair2(tableauDeTrous, tabUneFace,pointDebutFace,indexCourant) {
     tabUneFace.push(pointDebutFace, tableauDeTrous[indexCourant + 1], tableauDeTrous[indexCourant + 2]);
     indexCourant += 2;
@@ -270,6 +297,7 @@ function verifTabImpair(tableauDeTrous, pointDebutFace){
 
 
 function remplirGeometry(tableauUneFace) {
+    //completerStructure(tableauUneFace);
     //tableauUneFace = detectSensParcours(tableauUneFace);
     let positions = Array.from(geometry_model.getAttribute("position").array);
     //console.log(positions);
@@ -316,7 +344,7 @@ function repairFacesNormals() {
     }
 }
 
-function detectSensParcours(points) {
+/*function detectSensParcours(points) {
     geometry_model.computeVertexNormals();
     console.log("Detect sens parcours");
     console.log(points);
@@ -333,7 +361,7 @@ function detectSensParcours(points) {
    // let normal = geometry_model.getAttribute('normal');
     /*let valeurNormal = normal.x * v1.x + normal.y * v1.y + normal.z * v1.z;
     console.log("valeur normale : ", valeurNormal);*/
-    let normal = new THREE.Vector3().crossVectors(edge1, edge2).normalize();
+    /*let normal = new THREE.Vector3().crossVectors(edge1, edge2).normalize();
     console.log(normal);
     console.log(normal.dot(v1))
     if(normal.dot(v1) < 0){
@@ -344,5 +372,5 @@ function detectSensParcours(points) {
         console.log("points vers l'extérieur")
         return points;
     }
-}
+}*/
 
--- js/structure/Mesh.js
+++ js/structure/Mesh.js
@@ -1,3 +1,5 @@
+import {mesh} from "../tool/Element3DGeneraux";
+
 export class Mesh {
     constructor(faces) {
         this.faces = faces;
@@ -14,23 +16,75 @@ Mesh.prototype.getFacesFromVertex = function (vertex) {
  * @returns {*}
  */
 Mesh.prototype.getHalfedgeOfVertexWithoutOpposite = function(sommet){
+    console.log("dans recherche de l'opposee 1 sommet")
     let halfedge;
     let i=0;
     let halfedgeCourante;
-    while (typeof halfedge === "undefined" && i < this.faces.length){
-        halfedgeCourante = this.faces[i].edge;
+    while (typeof halfedge === "undefined" && i < mesh.faces.length){
+        halfedgeCourante = mesh.faces[i].edge;
         //console.log(halfedgeCourante.vertex.point, sommet.point)
-        if(halfedgeCourante.vertex.point.equals(sommet.point) &&
+        if(halfedgeCourante.vertex.equals(sommet) &&
             (halfedgeCourante.opposite === null || typeof halfedgeCourante.opposite === "undefined")){
             halfedge = halfedgeCourante;
-        } else if (halfedgeCourante.next.vertex.point.equals(sommet.point) &&
-            (halfedgeCourante.opposite === null || typeof halfedgeCourante.opposite === "undefined")){
+            return halfedgeCourante;
+        } else if (halfedgeCourante.next.vertex.equals(sommet) &&
+            (halfedgeCourante.next.opposite === null || typeof halfedgeCourante.next.opposite === "undefined")){
             halfedge = halfedgeCourante.next;
-        } else if (halfedgeCourante.prev.vertex.point.equals(sommet.point) &&
-            (halfedgeCourante.opposite === null || typeof halfedgeCourante.opposite === "undefined"))
+            return halfedgeCourante.next;
+        } else if (halfedgeCourante.prev.vertex.equals(sommet) &&
+            (halfedgeCourante.prev.opposite === null || typeof halfedgeCourante.prev.opposite === "undefined")){
+            halfedge = halfedgeCourante.prev;
+            return halfedgeCourante.prev;
+        }
         i++;
     }
     if(typeof halfedge === "undefined")
         console.log("aucune halfedge à ce sommet sans opposée")
+    return halfedge;
+}
+
+Mesh.prototype.getHalfedgeBetween2VertexWithoutOpposite = function(sommet1, sommet2){
+    console.log("dans recherche de l'opposee 2 sommets", sommet1, sommet2)
+    let halfedge;
+    let i=0;
+    let halfedgeCourante;
+    let p1Courant;
+    let p2Courant;
+    let tabP = [];
+    while (typeof halfedge === "undefined" && i < mesh.faces.length){
+        halfedgeCourante = mesh.faces[i].edge;
+        tabP.push(halfedgeCourante.vertex, halfedgeCourante.next.vertex);
+        console.log("tabP 1 : ", tabP.includes(sommet1)&&tabP.includes(sommet2))
+        if (tabP.includes(sommet1)&&tabP.includes(sommet2)&&
+            (halfedgeCourante.opposite === null || typeof halfedgeCourante.opposite === "undefined")){
+            console.log("halfedgeDeH2", halfedgeCourante);
+
+            return halfedgeCourante;
+        }
+        tabP = [];
+        tabP.push(halfedgeCourante.next.vertex, halfedgeCourante.next.next.vertex);
+        
+        console.log("tabP 2 : ", tabP.includes(sommet1)&&tabP.includes(sommet2))
+
+        if (tabP.includes(sommet1)&&tabP.includes(sommet2)&&
+            (halfedgeCourante.next.opposite === null || typeof halfedgeCourante.next.opposite === "undefined")){
+            console.log("halfedgeDeH2", halfedgeCourante.next);
+
+            return halfedgeCourante.next;
+        }
+        tabP = [];
+        tabP.push(halfedgeCourante.prev.vertex, halfedgeCourante.vertex);
+        console.log("tabP 3 : ", tabP.includes(sommet1)&&tabP.includes(sommet2))
+
+        if (tabP.includes(sommet1)&&tabP.includes(sommet2)&&
+            (halfedgeCourante.prev.opposite === null || typeof halfedgeCourante.prev.opposite === "undefined")){
+            console.log("halfedgeDeH2", halfedgeCourante.prev);
+
+            return halfedgeCourante.prev;
+        }
+        i++;
+    }
+    if(typeof halfedge === "undefined")
+        console.log("aucune halfedge à ces sommets sans opposée")
     return halfedge;
 }
