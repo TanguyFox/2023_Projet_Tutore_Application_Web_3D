a7e4fdc3de6718be7a663075c962f014790fff66
TanguyFox <tanguyfox@gmail.com>
2024-04-09 10:36:51 +0200

    Try fix doc redirection

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import {defineConfig} from "vite";
-
+import {resolve} from "path";
 export default defineConfig({
     base : "/2023_Projet_Tutore_Application_Web_3D",
-    proxy : {
-        "2023_Projet_Tutore_Application_Web_3D" : "index.html",
-        "/2023_Projet_Tutore_Application_Web_3D" : "documentation.html",
-    }
-
 });
